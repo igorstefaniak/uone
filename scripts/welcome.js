4cec793aaa25e6d91533b9474b9a2f72671d8845
Igor Stefaniak <52112899+igorstefaniak@users.noreply.github.com>
2022-09-21 22:45:10 +0200

    -

---
+++ scripts/welcome.js
@@ -0,0 +1,4 @@
+const welcometext = ["Cześć👋", "Witaj ponownie👋"];
+let hellotext = document.getElementById('hello-text');
+hellotext.append()
+setAttribute('href',`./images/icons/favicon${Math.floor(10*Math.random())+1}.png`);
