913fbb95fc107cd20d8cd4246fe72b6d3f4fc7ca
Igor Stefaniak <52112899+igorstefaniak@users.noreply.github.com>
2022-09-24 22:05:51 +0200

    -

--- scripts/status.js
+++ scripts/status.js
@@ -5,22 +5,3 @@ window.addEventListener('offline', (e) => {
 window.addEventListener('online', (e) => {
     console.log("🟢 Połączenie przywrócone");
 });
-
-function getSelectedText() {
-    var selectedText = '';
-
-    // window.getSelection
-    if (window.getSelection) {
-        selectedText = window.getSelection();
-    }
-    // document.getSelection
-    else if (document.getSelection) {
-        selectedText = document.getSelection();
-    }
-    // document.selection
-    else if (document.selection) {
-        $(this).css("color", "#21fc6b").text("online");
-        console.log;
-    } else return;
-    document.testform.selectedtext.value = selectedText;
-}
