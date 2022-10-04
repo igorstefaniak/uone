b6da283c152393384ee645459a86201f7709bcac
Igor Stefaniak <52112899+igorstefaniak@users.noreply.github.com>
2022-10-04 22:37:33 +0200

    -

--- scripts/search.js
+++ scripts/search.js
@@ -1,30 +1,58 @@
-$('#dropdown').addClass('_vanish');
-$('#search-input').focus(
-    function(){
 
-            $('#dropdown').removeClass('_vanish');
+$('#alert').css('display', 'none')
+
+$('#search-input').focus(
+  function(){
+    $('#dropdown').removeClass('_vanish');
+  }
+)
+
+$('#search-input').focusout(
+  function(){
+    $('#dropdown').addClass('_vanish');
+  }
+)
+
+/* $('#search-input').focus(
+    function(){
+      $('#dropdown').mouseover(function(){
+        $('#dropdown').removeClass('_vanish');
+      }
+    )
+      $('#dropdown').removeClass('_vanish');
         }
 
 )
-$('#search-input').focusout(
+  $('#search-input').focusout (function(){
+    $('#dropdown').mouseleave(
     function(){
-            $('#dropdown').addClass('_vanish');
-        
+      $('#dropdown').addClass('_vanish');
     }
+    )
+  }
 )
+ */
 
 function filterFunction() {
     var input, filter, a, i;
     input = document.getElementById("search-input");
     filter = input.value.toUpperCase();
+    alert = document.getElementById("alert")
     div = document.getElementById("list");
-    a = div.getElementsByTagName("a");
+    a = div.getElementsByTagName("li");
+    n = 0;
     for (i = 0; i < a.length; i++) {
       txtValue = a[i].textContent || a[i].innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         a[i].style.display = "";
-      } else {
+        alert.style.display = "none";
+      } 
+      else {
         a[i].style.display = "none";
+        n++;
+          if(n == a.length){
+            alert.style.display = "";
+          }
       }
     }
-  }
+}
